17224f3ed5fddec0e155d6a3b82872d0e94f771a
HanoonIH <hanoonih123@gmail.com>
2020-07-26 20:36:54 +0530

    #1 color generator

---
+++ #1_Change_BG/app.js
@@ -0,0 +1,186 @@
+
+const colorBox = [
+    "AliceBlue",
+    "AntiqueWhite",
+    "Aqua",
+    "Aquamarine",
+    "Azure",
+    "Beige",
+    "Bisque",
+    "Black",
+    "BlanchedAlmond",
+    "Blue",
+    "BlueViolet",
+    "Brown",
+    "BurlyWood",
+    "CadetBlue",
+    "Chartreuse",
+    "Chocolate",
+    "Coral",
+    "CornflowerBlue",
+    "Cornsilk",
+    "Crimson",
+    "Cyan",
+    "DarkBlue",
+    "DarkCyan",
+    "DarkGoldenRod",
+    "DarkGray",
+    "DarkGrey",
+    "DarkGreen",
+    "DarkKhaki",
+    "DarkMagenta",
+    "DarkOliveGreen",
+    "DarkOrange",
+    "DarkOrchid",
+    "DarkRed",
+    "DarkSalmon",
+    "DarkSeaGreen",
+    "DarkSlateBlue",
+    "DarkSlateGray",
+    "DarkSlateGrey",
+    "DarkTurquoise",
+    "DarkViolet",
+    "DeepPink",
+    "DeepSkyBlue",
+    "DimGray",
+    "DimGrey",
+    "DodgerBlue",
+    "FireBrick",
+    "FloralWhite",
+    "ForestGreen",
+    "Fuchsia",
+    "Gainsboro",
+    "GhostWhite",
+    "Gold",
+    "GoldenRod",
+    "Gray",
+    "Grey",
+    "Green",
+    "GreenYellow",
+    "HoneyDew",
+    "HotPink",
+    "IndianRed",
+    "Indigo",
+    "Ivory",
+    "Khaki",
+    "Lavender",
+    "LavenderBlush",
+    "LawnGreen",
+    "LemonChiffon",
+    "LightBlue",
+    "LightCoral",
+    "LightCyan",
+    "LightGoldenRodYellow",
+    "LightGray",
+    "LightGrey",
+    "LightGreen",
+    "LightPink",
+    "LightSalmon",
+    "LightSeaGreen",
+    "LightSkyBlue",
+    "LightSlateGray",
+    "LightSlateGrey",
+    "LightSteelBlue",
+    "LightYellow",
+    "Lime",
+    "LimeGreen",
+    "Linen",
+    "Magenta",
+    "Maroon",
+    "MediumAquaMarine",
+    "MediumBlue",
+    "MediumOrchid",
+    "MediumPurple",
+    "MediumSeaGreen",
+    "MediumSlateBlue",
+    "MediumSpringGreen",
+    "MediumTurquoise",
+    "MediumVioletRed",
+    "MidnightBlue",
+    "MintCream",
+    "MistyRose",
+    "Moccasin",
+    "NavajoWhite",
+    "Navy",
+    "OldLace",
+    "Olive",
+    "OliveDrab",
+    "Orange",
+    "OrangeRed",
+    "Orchid",
+    "PaleGoldenRod",
+    "PaleGreen",
+    "PaleTurquoise",
+    "PaleVioletRed",
+    "PapayaWhip",
+    "PeachPuff",
+    "Peru",
+    "Pink",
+    "Plum",
+    "PowderBlue",
+    "Purple",
+    "RebeccaPurple",
+    "Red",
+    "RosyBrown",
+    "RoyalBlue",
+    "SaddleBrown",
+    "Salmon",
+    "SandyBrown",
+    "SeaGreen",
+    "SeaShell",
+    "Sienna",
+    "Silver",
+    "SkyBlue",
+    "SlateBlue",
+    "SlateGray",
+    "SlateGrey",
+    "Snow",
+    "SpringGreen",
+    "SteelBlue",
+    "Tan",
+    "Teal",
+    "Thistle",
+    "Tomato",
+    "Turquoise",
+    "Violet",
+    "Wheat",
+    "White",
+    "WhiteSmoke",
+    "Yellow",
+    "YellowGreen",
+  ];
+function convert(rgb) {  
+    rgb = rgb.match(/^rgb\((\d+), \s*(\d+), \s*(\d+)\)$/);  
+    function hexCode(i) {  
+        
+        // Take the last 2 characters and convert 
+        // them to Hexadecimal. 
+        return ("0" + parseInt(i).toString(16)).slice(-2); 
+    }  
+    return "#" + hexCode(rgb[1]) + hexCode(rgb[2])  
+                + hexCode(rgb[3]);  
+}
+
+function changeBg() {
+
+    var backGround = document.querySelector('body');
+    let randomIndex = parseInt(Math.random() * (colorBox.length))
+    backGround.style.backgroundColor = colorBox[randomIndex]
+
+    var display = document.getElementById('display');
+    var hexDisplay = document.getElementById('hexDisplay');
+    var rgbDisplay = document.getElementById('rgbDisplay');
+
+    display.textContent = colorBox[randomIndex]
+    hexDisplay.textContent = convert(window.getComputedStyle(backGround).backgroundColor); 
+    rgbDisplay.textContent = window.getComputedStyle(backGround).backgroundColor;
+
+    var css = document.getElementById('css')
+    var hex = document.getElementById('hex')
+    var rgb = document.getElementById('rgb')
+
+    const CSS_SHOW =  css.checked ? display.style.display = "block" : display.style.display = "none";
+    const HEX_SHOW =  hex.checked ? hexDisplay.style.display = "block" : hexDisplay.style.display = "none";
+    const RGB_SHOW =  rgb.checked ? rgbDisplay.style.display = "block" : rgbDisplay.style.display = "none";
+}
+ 
